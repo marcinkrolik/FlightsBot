645a8ba8751a7b90fd0a1d011a490b9c1db30c68
Marcin Krolik <kromar.box@gmail.com>
2013-06-26 13:14:23 +0200

    mail added

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from SkyScannerTest import *
+import boto.ses
+import os
 
 def main():
     
-    d = (("GDN", "MNL", "10.07.2013", "10.08.2013"), ("GDN", "CGK", "10.07.2013", "10.08.2013"))
+    body = ''
+    
+    data = (("GDN", "MNL", "10.07.2013", "10.08.2013"), ("GDN", "CGK", "10.07.2013", "10.08.2013"))
     test = SkyScannerTest()
     
-    test.setUp()
-    r1 = test.test(d[0])
-    test.tearDown()
+    for d in data:
+        test.setUp()
+        body += "%s - %s \n%s <-> %s : %s \n" % (d[3], d[4], d[0], d[1], ";".join(test.test(d)))
+        test.tearDown()
     
-    test.setUp()
-    r2 = test.test(d[1])
-    test.tearDown()
+    sendMail(body)    
+    #test.setUp()
+    #r2 = test.test(d[1])
+    #test.tearDown()
     
-    print r1
-    print r2
+    #print r1
+    #print r2
+def sendMail(body):
+    
+    REGION = 'us-east-1'
+    FROM = 'kromar.box@gmail.com'
+    SUBJECT = 'Your subject'
+    BODY = 'Body here'
+    TO = ['marcin.krolik@gmail.com']#, 'zmudakat@gmail.com']
+    
+    conn = boto.ses.connect_to_region(REGION, aws_access_key_id = os.environ['AWSKEY'], aws_secret_access_key = os.environ['AWSPASS'])
+    conn.send_email(FROM, SUBJECT, body, TO)
 
 if __name__ == '__main__':
     main()
