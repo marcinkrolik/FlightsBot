df4c536d3cfc331d1dd58fb5290f17863eb7cfe6
Marcin Krolik <kromar.box@gmail.com>
2013-06-26 13:35:05 +0200

    testing some new entries and mail to kasia

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ def main():
     
     body = ''
     
-    data = (("GDN", "MNL", "10.07.2013", "10.08.2013"), ("GDN", "CGK", "10.07.2013", "10.08.2013"))
+    data = (("GDN", "MNL", "10.07.2013", "10.08.2013"),
+            ("GDN", "CGK", "10.07.2013", "10.08.2013"),
+            ("WAW", "CGK", "10.08.2013", "10.09.2013"),
+            ("WAW", "CGK", "10.09.2013", "10.10.2013"))
     test = SkyScannerTest()
     
     for d in data:
         test.setUp()
-        body += "%s - %s \n%s <-> %s : %s \n" % (d[3], d[4], d[0], d[1], ";".join(test.test(d)))
+        body += "%s - %s \n%s <-> %s : %s \n" % (d[2], d[3], d[0], d[1], ";".join(test.test(d)))
         test.tearDown()
-    
+    body += "\n\nPrepared by fully automated Kromar"
     sendMail(body)    
     #test.setUp()
     #r2 = test.test(d[1])
@@ -27,7 +30,7 @@ def sendMail(body):
     FROM = 'kromar.box@gmail.com'
     SUBJECT = 'Your subject'
     BODY = 'Body here'
-    TO = ['marcin.krolik@gmail.com']#, 'zmudakat@gmail.com']
+    TO = ['marcin.krolik@gmail.com', 'zmudakat@gmail.com']
     
     conn = boto.ses.connect_to_region(REGION, aws_access_key_id = os.environ['AWSKEY'], aws_secret_access_key = os.environ['AWSPASS'])
     conn.send_email(FROM, SUBJECT, body, TO)
